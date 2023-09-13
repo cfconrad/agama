1f1bd3bd02003879cdb2fbdb1afdd7f607a98e3c
Jorik Cronenberg <jcronenberg@suse.de>
2023-09-13 13:08:02 +0200

    Rustfmt

--- rust/agama-migrate-wicked/src/interface.rs
+++ rust/agama-migrate-wicked/src/interface.rs
@@ -1,7 +1,7 @@
-use std::str::FromStr;
-use serde::{Deserialize, Serialize};
-use agama_dbus_server::network::model::{self, IpAddress, Ipv4Config, IpMethod};
+use agama_dbus_server::network::model::{self, IpAddress, IpMethod, Ipv4Config};
 use agama_lib::network::types::DeviceType;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
 #[serde(default)]
@@ -26,13 +26,11 @@ pub struct Control {
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
 #[serde(default)]
-pub struct Firewall {
-}
+pub struct Firewall {}
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
 #[serde(default)]
-pub struct Link {
-}
+pub struct Link {}
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
 #[serde(default)]
@@ -80,12 +78,21 @@ impl Into<model::Connection> for Interface {
 
 impl Into<Ipv4Config> for Interface {
     fn into(self) -> Ipv4Config {
-        let method = if self.ipv4.enabled && self.ipv4_static.is_some() { "manual" }
-        else if !self.ipv4.enabled { "disabled" }
-        else { "auto" };
+        let method = if self.ipv4.enabled && self.ipv4_static.is_some() {
+            "manual"
+        } else if !self.ipv4.enabled {
+            "disabled"
+        } else {
+            "auto"
+        };
 
         let mut ipv4 = Ipv4Config::default();
-        if self.ipv4_static.is_some() { ipv4.addresses = vec![IpAddress::from_str(&self.ipv4_static.unwrap().address.local.as_str()).unwrap()] }
+        if self.ipv4_static.is_some() {
+            ipv4.addresses =
+                vec![
+                    IpAddress::from_str(&self.ipv4_static.unwrap().address.local.as_str()).unwrap(),
+                ]
+        }
         ipv4.method = IpMethod::from_str(method).unwrap();
 
         ipv4
@@ -99,20 +106,33 @@ mod tests {
     #[test]
     fn test_static_interface_to_connection() {
         let static_interface = Interface {
-            ipv4: Ipv4 { enabled: true, ..Default::default() },
-            ipv4_static: Some(Ipv4Static { address: Address{ local: "127.0.0.1/8".to_string() } }),
+            ipv4: Ipv4 {
+                enabled: true,
+                ..Default::default()
+            },
+            ipv4_static: Some(Ipv4Static {
+                address: Address {
+                    local: "127.0.0.1/8".to_string(),
+                },
+            }),
             ..Default::default()
         };
 
         let static_connection: model::Connection = static_interface.into();
         assert_eq!(static_connection.base().ipv4.method, IpMethod::Manual);
-        assert_eq!(static_connection.base().ipv4.addresses[0].to_string(), "127.0.0.1/8");
+        assert_eq!(
+            static_connection.base().ipv4.addresses[0].to_string(),
+            "127.0.0.1/8"
+        );
     }
 
     #[test]
     fn test_dhcp_interface_to_connection() {
         let static_interface = Interface {
-            ipv4: Ipv4 { enabled: true, ..Default::default() },
+            ipv4: Ipv4 {
+                enabled: true,
+                ..Default::default()
+            },
             ..Default::default()
         };
 
